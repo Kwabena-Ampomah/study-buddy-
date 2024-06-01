1d4071c9f37608b72d5b65cc692b6af09a89902e
kwabena-ampomah <kwabenaandampomah@gmail.com>
2024-06-01 17:19:49 -0400

    added the cards to connect to firebase also changed the purpose of the webapp

--- study-buddy/src/Header.js
+++ study-buddy/src/Header.js
@@ -1,20 +1,25 @@
 import React from "react";
 import "./Header.css";
 import { CgProfile } from "react-icons/cg";
-import { FaMessage } from "react-icons/fa6";
-import { BsFillBookmarkHeartFill } from "react-icons/bs";
+import { TbSoccerField } from "react-icons/tb";
+import { MdOutlineSportsSoccer } from "react-icons/md";
 function Header(){
     return(
         <div className="header">
-      <a href="/profile" className="header_icon">
+      <a href="/profile" className="header_icon burb">
+        <style>
+          backgroudn
+        </style>
         <CgProfile fontSize="40px" />
       </a>
       <a href="/bookmarks" className="header_icon heart">
-        <BsFillBookmarkHeartFill fontSize="40px" />
+      <MdOutlineSportsSoccer fontSize="40px" />
       </a>
-      <a href="/messages" className="header_icon">
-        <FaMessage fontSize="40px" />
+      <div className="righticon">
+      <a href="/play" className="header_icon fly">
+        <TbSoccerField fontSize="40px" />
       </a>
+      </div>
     </div>
     )
 }
--- study-buddy/src/TinderCards.js
+++ study-buddy/src/TinderCards.js
@@ -1,38 +1,65 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TinderCard from "react-tinder-card";
-import "./TinderCards.css"
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, onSnapshot } from 'firebase/firestore';
+import "./TinderCards.css";
 
+const firebaseConfig = {
+    apiKey: "AIzaSyDxPHjSUhbPYJVKwh35lsgPU3ea4QDtt54",
+    authDomain: "study-buddy-4d493.firebaseapp.com",
+    projectId: "study-buddy-4d493",
+    storageBucket: "study-buddy-4d493.appspot.com",
+    messagingSenderId: "608775470734",
+    appId: "1:608775470734:web:9fd95e23472c52ff8f53df",
+    measurementId: "G-KQ63CF5WEL"
+    };
+
+// Initialize Firebase and get the Firestore instance
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app);
 
 function TinderCards() {
-  const [people, setPeople] = useState([
-    {
-      name: 'bradley',
-      url: `/images/mbape.png`
-    },
-    {
-      name: "ivan",
-      url: `${process.env.PUBLIC_URL}/images/mbape.png`
-    }
-  ]);
+  const [people, setPeople] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(collection(firestore, 'people'), (snapshot) => {
+      setPeople(snapshot.docs.map(doc => doc.data()));
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const swiped = (direction, nameToDelete) => {
+    console.log("Removing: " + nameToDelete);
+    setPeople(people => people.filter(person => person.name !== nameToDelete));
+  };
+
+  const outOfFrame = (name) => {
+    console.log(name + " left the screen!");
+  };
 
   return (
     <div>
-      <h1>Tinder Cards</h1>
-      {people.map(person => (
-        <TinderCard
-          className="swipe"
-          key={person.name}
-          preventSwipe={['up','down']}
-
-        >
-          <div 
-            className="card" 
-            style={{ backgroundImage: `url(${person.url})` }}
+      <h1>Swipe-Squad</h1>
+      <div className="tinderCards_cardContainer">
+        {people.map((person, index) => (
+          <TinderCard
+            className="swipe"
+            key={person.name}
+            preventSwipe={['up', 'down']}
+            onSwipe={(dir) => swiped(dir, person.name)}
+            onCardLeftScreen={() => outOfFrame(person.name)}
           >
-            <h3>{person.name}</h3>
-          </div>
-        </TinderCard>
-      ))}
+            <div
+              className="card"
+              style={{ backgroundImage: `url(${person.url})` }}
+            >
+              <h3>{person.name}</h3>
+              <h1>{person.pos}</h1>
+            </div>
+          </TinderCard>
+        ))}
+      </div>
     </div>
   );
 }
