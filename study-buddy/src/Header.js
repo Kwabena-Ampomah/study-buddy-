be680b33b8d771bc6c45587e0a7200527e694005
kwabena-ampomah <kwabenaandampomah@gmail.com>
2024-08-23 14:09:29 -0400

    bs

--- study-buddy/src/Header.js
+++ study-buddy/src/Header.js
@@ -1,26 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Header.css";
 import { CgProfile } from "react-icons/cg";
 import { TbSoccerField } from "react-icons/tb";
 import { MdOutlineSportsSoccer } from "react-icons/md";
-function Header(){
-    return(
-        <div className="header">
-      <a href="/profile" className="header_icon burb">
-        <style>
-          backgroudn
-        </style>
+
+function Header() {
+  return (
+    <div className="header">
+      <Link to="/profile" className="header_icon burb">
         <CgProfile fontSize="40px" />
-      </a>
-      <a href="/bookmarks" className="header_icon heart">
-      <MdOutlineSportsSoccer fontSize="40px" />
-      </a>
+      </Link>
+      <Link to="/bookmarks" className="header_icon heart">
+        <MdOutlineSportsSoccer fontSize="40px" />
+      </Link>
       <div className="righticon">
-      <a href="/play" className="header_icon fly">
-        <TbSoccerField fontSize="40px" />
-      </a>
+        <Link to="/play" className="header_icon fly">
+          <TbSoccerField fontSize="40px" />
+        </Link>
       </div>
     </div>
-    )
+  );
 }
-export default Header
+
+export default Header;
